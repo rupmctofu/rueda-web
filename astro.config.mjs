b6ecff85697144df53075ad36092dc8d47575b36
verdulife <verdu@live.com>
2023-12-03 10:15:02 +0100

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [tailwind()],
+  adapter: netlify()
 });
