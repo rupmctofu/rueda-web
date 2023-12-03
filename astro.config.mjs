e3d68e847de2e698e47831c96f61068ce8ac8fc3
verdulife <verdu@live.com>
2023-12-03 10:24:26 +0100

    adapter 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService(),
-  },
   output: "hybrid",
   integrations: [tailwind()],
   adapter: netlify(),
