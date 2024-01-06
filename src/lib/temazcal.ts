3987c11b280b8cc5d6cc6f8fa9d4d3fc57f3fac8
rupmctofu <roberto.v.lopez@gmail.com>
2024-01-06 11:10:52 +0100

    rup canviar dates

--- src/lib/temazcal.ts
+++ src/lib/temazcal.ts
@@ -7,10 +7,10 @@ export const temazcal = {
   html: `Los Q'eros han visitado y compartido sus enseñanzas en Tarragona varias veces en los 10 años de escuela, los Pampamesayok, Don Alejandro Apaza, Doña Santusa P'aco Ñusta y Altomesayok, Doña María Apaza Quispe, única Kurac akullac auténtica del linaje y la tradición Q'ero y el antropólogo y estudiador de la cultura Q'ero Don Juan Nuñez del Prado y su hijo han impartido cosmología andina, en nuestro espacio en Salomó Tarragona.`,
   background: "bg-maestria",
   event: {
-    date: "22  DICIEMBRE - 16:00H",
-    title: "TEMAZCAL DEL SOLSTICIO DE INVIERNO",
+    date: "3 y 4 de febrero",
+    title: "TEMAZCAL",
     description:
-      "Recibiendo el invierno y templando nuestro fuego interno. *En esta ocasión solo se celebrará el temazcal, si quieres pasar la noche, contáctanos.",
+      "Recibiendo el invierno y templando nuestro fuego interno.</br> *En esta ocasión solo se celebrará el temazcal, si quieres pasar la noche, contáctanos.",
   },
   benefitsCards: [
     "Depura las vías respiratorias y el aparato digestivo.",
