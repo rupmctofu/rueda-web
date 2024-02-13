880eef93d92e11fe6fa4948ca1b0354b3ce72bc7
rupmctofu <roberto.v.lopez@gmail.com>
2024-02-13 12:34:11 +0100

    noves dates

--- src/lib/temazcal.ts
+++ src/lib/temazcal.ts
@@ -7,10 +7,10 @@ export const temazcal = {
   html: `Los Q'eros han visitado y compartido sus enseñanzas en Tarragona varias veces en los 10 años de escuela, los Pampamesayok, Don Alejandro Apaza, Doña Santusa P'aco Ñusta y Altomesayok, Doña María Apaza Quispe, única Kurac akullac auténtica del linaje y la tradición Q'ero y el antropólogo y estudiador de la cultura Q'ero Don Juan Nuñez del Prado y su hijo han impartido cosmología andina, en nuestro espacio en Salomó Tarragona.`,
   background: "bg-maestria",
   event: {
-    date: "3 y 4 de febrero",
-    title: "TEMAZCAL empezará a las 12:00 el día 3 y finalizará el 4 a las 12:00.",
+    date: "13 y 14 de Abril",
+    title: "TEMAZCAL empezará a las 12:00 el día 13 y finalizará el 14 a las 12:00.",
     description:
-      "Temazcal, cena, dormir, desayuno y sanación chamánica",
+      "Temazcal+ hatun Karpai (iniciación muy poderosa que da sabiduria y energía)+ cena + dormir+ sanación grupal.",
   },
   benefitsCards: [
     "Depura las vías respiratorias y el aparato digestivo.",
