776b74cd037a795f5e351e7202a54e81032521b8
rup <roberto.v.lopez@gmail.com>
2024-10-18 16:37:29 +0200

    More Fechas 2025

--- src/lib/temazcal.ts
+++ src/lib/temazcal.ts
@@ -7,11 +7,17 @@ export const temazcal = {
   html: `Los Q'eros han visitado y compartido sus enseñanzas en Tarragona varias veces en los 10 años de escuela, los Pampamesayok, Don Alejandro Apaza, Doña Santusa P'aco Ñusta y Altomesayok, Doña María Apaza Quispe, única Kurac akullac auténtica del linaje y la tradición Q'ero y el antropólogo y estudiador de la cultura Q'ero Don Juan Nuñez del Prado y su hijo han impartido cosmología andina, en nuestro espacio en Salomó Tarragona.`,
   background: "bg-maestria",
   event: {
-    date: "21 de Diciembre & 21 de Enero a las 12h",
-    title: "TEMAZCAL",
+    date: "27 de Octubre",
+    title: "TEMAZCAL MUJERES",
     description:
-      "Solsticio de invierno",
-  },
+      "Temazcal exlusivo para mujeres",
+    },
+    event2: {
+      date: "21 de Diciembre a las 12h",
+      title: "TEMAZCAL",
+      description:
+        "Solsticio de invierno",
+      },
   benefitsCards: [
     "Depura las vías respiratorias y el aparato digestivo.",
     "Tonifica el sistema nervioso.",
