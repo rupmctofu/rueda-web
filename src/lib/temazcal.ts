7d516b151c972a94f268a4c36d2cb3be77e052b6
rup <roberto.v.lopez@gmail.com>
2024-10-30 21:07:47 +0100

    Temazcal 10 nov

--- src/lib/temazcal.ts
+++ src/lib/temazcal.ts
@@ -7,7 +7,7 @@ export const temazcal = {
   html: `Los Q'eros han visitado y compartido sus enseñanzas en Tarragona varias veces en los 10 años de escuela, los Pampamesayok, Don Alejandro Apaza, Doña Santusa P'aco Ñusta y Altomesayok, Doña María Apaza Quispe, única Kurac akullac auténtica del linaje y la tradición Q'ero y el antropólogo y estudiador de la cultura Q'ero Don Juan Nuñez del Prado y su hijo han impartido cosmología andina, en nuestro espacio en Salomó Tarragona.`,
   background: "bg-maestria",
   event: {
-    date: "27 de Octubre",
+    date: "10 de Nobiembre",
     title: "TEMAZCAL MIXTO",
     description:
       "A las 12h",
