f1fc4351ccc66db329eb09ba557ed8365448be7f
rup <roberto.v.lopez@gmail.com>
2025-05-21 15:39:31 +0200

    fechas cambiadas en mayo

--- src/lib/temazcal.ts
+++ src/lib/temazcal.ts
@@ -7,8 +7,8 @@ export const temazcal = {
   html: `Los Q'eros han visitado y compartido sus enseñanzas en Tarragona varias veces en los 10 años de escuela, los Pampamesayok, Don Alejandro Apaza, Doña Santusa P'aco Ñusta y Altomesayok, Doña María Apaza Quispe, única Kurac akullac auténtica del linaje y la tradición Q'ero y el antropólogo y estudiador de la cultura Q'ero Don Juan Nuñez del Prado y su hijo han impartido cosmología andina, en nuestro espacio en Salomó Tarragona.`,
   background: "bg-maestria",
       event: {
-        date: "17 y 18 de mayo",
-        title: "TEMAZCAL",
+        date: "21 de Junio",
+        title: "Celebraremos el solsticio",
         description: "Mixto",
         },
         
