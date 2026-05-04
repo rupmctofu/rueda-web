515455eeca6583832149355a445cd0eba520a43a
rup <roberto.v.lopez@gmail.com>
2026-05-04 20:18:29 +0200

    cambio temazcales

--- src/lib/directions.ts
+++ src/lib/directions.ts
@@ -98,7 +98,7 @@ export const directions = [
     title: "norte",
     subtitle: "COLIBRÍ",
     description: "La dirección del y la maestra interna",
-    date: "30 Abril a 3 Mayo 2026",
+    date: "Fechas por confirmar",
     html: `En esta dirección alegre, emocional, los roles, los maestros y el compromiso, TU COMPROMISO contigo, estará muy presente y podrás observar y <strong class="font-semibold">reconocer el o la maestra sabia interna que está en ti.</strong>`,
     background: "bg-norte",
     footer: "CONSEGUIRÁS",
--- src/lib/temazcal.ts
+++ src/lib/temazcal.ts
@@ -7,15 +7,10 @@ export const temazcal = {
   html: `Los Q'eros han visitado y compartido sus enseñanzas en Tarragona varias veces en los 10 años de escuela, los Pampamesayok, Don Alejandro Apaza, Doña Santusa P'aco Ñusta y Altomesayok, Doña María Apaza Quispe, única Kurac akullac auténtica del linaje y la tradición Q'ero y el antropólogo y estudiador de la cultura Q'ero Don Juan Nuñez del Prado y su hijo han impartido cosmología andina, en nuestro espacio en Salomó Tarragona.`,
   background: "bg-maestria",
       event: {
-        date: "18 y 19 Abril 2026",
+        date: "23 y 24 de Mayo 2026",
         title: "TEMAZCAL MIXTO",
         description: "",
-        },
-      event2: {
-        date: "20 y 21 Junio 2026",
-        title: "SOLSTICIO DE VERANO",
-        description: ""},
-
+        },  
         
   benefitsCards: [
     "Depura las vías respiratorias y el aparato digestivo.",
